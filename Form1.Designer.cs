a45001c2ac01bf85f38fcd158dc1c2d3027fd2ea
Drunkendragonify <michael.clark@utcreading.org.uk>
2018-04-02 16:44:54 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SplitCloudClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.selectfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 405);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(469, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // selectfile
+            // 
+            this.selectfile.Location = new System.Drawing.Point(12, 12);
+            this.selectfile.Name = "selectfile";
+            this.selectfile.Size = new System.Drawing.Size(130, 69);
+            this.selectfile.TabIndex = 1;
+            this.selectfile.Text = "Select File";
+            this.selectfile.UseVisualStyleBackColor = true;
+            this.selectfile.Click += new System.EventHandler(this.Selectfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 440);
+            this.Controls.Add(this.selectfile);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Split Cloud Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button selectfile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitCloudClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Selectfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Curseor Files|*.cur";
+            openFileDialog1.Title = "Select a Cursor File";
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Cursor = new Cursor(openFileDialog1.OpenFile());
+            }
+        }
+    }
+}
